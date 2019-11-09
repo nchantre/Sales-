5d2f48985f72fc7a3238b329a64cef068c00b089
nchantre <43796086+nchantre@users.noreply.github.com>
2019-11-09 11:56:01 -0500

    Configuracion En android
    Configuracion Android

--- Sales/Sales/App.xaml.cs
+++ Sales/Sales/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Sales.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,8 @@ namespace Sales
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            // MainPage = new NavigationPage (new MainPage());
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
--- Sales/Sales/MainPage.xaml.cs
+++ Sales/Sales/MainPage.xaml.cs
@@ -16,6 +16,25 @@ namespace Sales
         public MainPage()
         {
             InitializeComponent();
+            Inicializar();
+        }
+
+        private void Inicializar()
+        {
+            buttonTexto.Clicked += ButtonTexto_Clicked;
+            btnSiguiente.Clicked += BtnSiguiente_Clicked;
+            //throw new NotImplementedException();
+        }
+
+        private void BtnSiguiente_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new OtraPagina());
+        }
+
+        private void ButtonTexto_Clicked(object sender, EventArgs e)
+        {
+            labelTexto.Text = entryTexto.Text;
+         
         }
     }
 }
