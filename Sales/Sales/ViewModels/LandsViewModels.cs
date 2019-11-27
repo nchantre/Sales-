74077475045b6406de3ccb378428eb601f05a413
nchantre <43796086+nchantre@users.noreply.github.com>
2019-11-26 23:11:18 -0500

    List View Personalizada
    Personalizada Conexion Con el APi

--- Sales/Sales/ViewModels/LandsViewModels.cs
+++ Sales/Sales/ViewModels/LandsViewModels.cs
@@ -4,10 +4,11 @@ using Sales.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using GalaSoft.MvvmLight.Command;
-
 using System.Text;
 using Xamarin.Forms;
+using System.Linq;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 
 namespace Sales.ViewModels
 {
@@ -19,6 +20,9 @@ namespace Sales.ViewModels
         #endregion
         #region Attributes
         private ObservableCollection<Land> lands;
+        private bool isRefreshing;
+        private string filter;
+        private List<Land> landsList;
         #endregion
 
         #region  Properties
@@ -28,6 +32,22 @@ namespace Sales.ViewModels
             set { SetValue(ref this.lands, value); }
 
         }
+        public bool IsRefreshing
+        {
+            get { return isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+
+        }
+        public string Filter
+        {
+            get { return filter; }
+            set
+            { 
+                SetValue(ref this.filter, value);
+                this.Search();
+            }
+
+        }
         #endregion
 
         #region Constructors
@@ -35,6 +55,7 @@ namespace Sales.ViewModels
         {
             this.apiService = new ApiService();
             this.LoadLands();
+     
         }
 
 
@@ -43,10 +64,11 @@ namespace Sales.ViewModels
         #region Methods
         private async void LoadLands()
         {
-
+            this.IsRefreshing = true;
             var connection = await this.apiService.CheckConnection();
             if (!connection.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
                     connection.Message,
@@ -71,10 +93,46 @@ namespace Sales.ViewModels
 
             }
 
-            var list = (List < Land > )response.Result;
-            this.Lands = new ObservableCollection<Land>(list);
+            this.landsList = (List < Land > )response.Result;
+            this.Lands = new ObservableCollection<Land>(this.landsList);
+            this.IsRefreshing = false;
+
+        }
+        #endregion
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }
+
+        }
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            }
+
+        }
+
+        private void Search()
+        {
+            if (string.IsNullOrEmpty(this.Filter))
+            {
+                this.Lands = new ObservableCollection<Land>(this.landsList);
+            }
+            else
+            {
+                this.Lands = new ObservableCollection<Land>(
+                   this.landsList.Where(
+                       l => l.Name.ToLower().Contains(this.Filter.ToLower()) ||
+                            l.Capital.ToLower().Contains(this.Filter.ToLower())));
+            }
             
         }
+
         #endregion
     }
 }
